92cd22be22275a69811fa8c71ff9427f11e1f2ed
Evgen <ebabin000@gmail.com>
2026-07-21 16:53:23 +0300

    turbo configs

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,4 @@
+import { z } from "zod";
+
+export const propertyDealTypeSchema = z.enum(["SALE", "LONG_TERM_RENT", "DAILY_RENT"]);
+export type PropertyDealType = z.infer<typeof propertyDealTypeSchema>;
